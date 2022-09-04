6bbfd807665f5a746dabd54afda1d5a982c464a3
Matt Russell <mrussell@cs.tufts.edu>
2022-09-04 15:53:42 -0400

    first commit

---
+++ .devcontainer/prep-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+COURSE_NUM="40"
+LOCAL_FOLDER=`basename "$PWD"`
+
+correct="n"
+
+mkdir .vscode > /dev/null 2>&1
+
+cp .devcontainer/json_templates/sftp.json .vscode/sftp.json
+cp .devcontainer/json_templates/devcontainer.json .devcontainer/devcontainer.json
+
+while [[ ${correct} != "y" ]]; do    
+    echo "Please enter your utln for homework.cs.tufts.edu"
+    read UTLN
+    echo "You wrote ${UTLN}"
+    echo "Was this correct? (y/n)"
+    read correct
+
+    while [[ ${correct} != "y" && ${correct} != "n" ]]; do
+        echo "please enter y or n"
+        read correct
+    done
+
+    if [[ ${correct} == "y" ]]; then
+        echo "success! if you entered the wrong info, please run this script again"
+    fi
+done
+
+sed -i'' -e "s/\$UTLN/${UTLN}/g" .devcontainer/devcontainer.json
+sed -i'' -e "s/\$UTLN/${UTLN}/g" .vscode/sftp.json
+
+sed -i'' -e "s/\$COURSE_NUM/${COURSE_NUM}/g" .devcontainer/devcontainer.json
+sed -i'' -e "s/\$COURSE_NUM/${COURSE_NUM}/g" .vscode/sftp.json
+
+sed -i'' -e "s/\$LOCAL_FOLDER/${LOCAL_FOLDER}/g" .devcontainer/devcontainer.json
+sed -i'' -e "s/\$LOCAL_FOLDER/${LOCAL_FOLDER}/g" .vscode/sftp.json
+
